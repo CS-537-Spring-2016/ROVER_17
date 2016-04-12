ad4970cef4a94261634ee84f5db9602994a428e1
Chittaksh <khadse.chittaksh@gmail.com>
2016-04-12 16:21:15 -0700

    Rover 17 registered and moving.

--- src/common/RoverLocations.java
+++ src/common/RoverLocations.java
@@ -36,6 +36,7 @@ public class RoverLocations {
 		// test rovers
 		roverHash.put(RoverName.ROVER_00, new Coord(4,20));
 		roverHash.put(RoverName.ROVER_99, new Coord(4,7));
+		roverHash.put(RoverName.ROVER_17, new Coord(0,0));
 	}
 	
 	public synchronized boolean moveRover(RoverName rname, Coord loc){
