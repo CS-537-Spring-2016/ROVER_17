54373dbb7bd59670e03903b67f608b9a978fe325
Steve Shim <steveyshim@gmail.com>
2016-05-05 19:16:00 -0700

    Helpers for A Star

---
+++ src/swarmBots/Rover17Utilities/Node.java
@@ -0,0 +1,55 @@
+package swarmBots.Rover17Utilities;
+
+
+public class Node<T> {
+    private int id;
+    private T data;
+
+    public Node(int id) {
+        this.id = id;
+        this.data = null;
+    }
+    public Node(int id, T data) {
+        this.id = id;
+        this.data = data;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Node)) return false;
+
+        Node node = (Node) o;
+
+        return getId() == node.getId();
+
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "id=" + id +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return getId();
+    }
+}
