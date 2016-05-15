c70cd7ac2192d464c56fa360bd3fe5604938f088
rkeshishians <rafik_ke@ymail.com>
2016-05-15 15:14:13 -0700

    work on repeat counter

--- src/swarmBots/Rover17Utilities/CountRepeats.java
+++ src/swarmBots/Rover17Utilities/CountRepeats.java
@@ -1,5 +1,23 @@
 package swarmBots.Rover17Utilities;
 
 public class CountRepeats {
+	
+	public void setTile(){
+		
+	}
 
+	public void isRedundat(){
+		
+	}
+}
+
+class Tile {
+	Tile tile;
+	String xCord;
+	String yCord;
+	
+	public Tile(String x, String y){
+		this.xCord = x;
+		this.yCord = y;		
+	}
 }
