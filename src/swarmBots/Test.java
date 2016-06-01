9bda71492bcc48bc391357b8da81b9ee6fc31946
Chittaksh <khadse.chittaksh@gmail.com>
2016-06-01 11:27:56 -0700

    test 2

--- src/swarmBots/Test.java
+++ src/swarmBots/Test.java
@@ -2,9 +2,11 @@ package swarmBots;
 
 public class Test {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-
+	public static void printHello(){
+		System.out.println("Print Hello World.");
 	}
 
+	public static void printDirection(String dir){
+		System.out.print(dir);
+	}
 }
